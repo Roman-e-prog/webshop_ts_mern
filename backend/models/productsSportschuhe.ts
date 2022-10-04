f596bc84c4f635d28a0f1820c3739f62fb68b00f
Roman <roman.rostock@gmail.com>
2022-10-04 15:46:12 +0200

    created Model ProductsSportschuhe

---
+++ backend/models/productsSportschuhe.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+interface ProductsSportschuheDocument extends mongoose.Document{
+    img:string,
+    cloudinary_id:string,
+    title:string,
+    producer:string,
+    category:string,
+    desc:string,
+    price:number,
+    currency:string,
+    colors:string[],
+    sizes:string[],
+    inStock:boolean,
+    createdAt: Date;
+    updatedAt: Date;
+}
+const ProductsSportschuheSchema = new mongoose.Schema<ProductsSportschuheDocument>({
+    img:{type:String, required:true},
+    cloudinary_id:{type:String, required:true},
+    title:{type:String, required:true},
+    producer:{type:String, required:true},
+    category:{type:String, required:true},
+    desc:{type:String, required:true},
+    price:{type:Number, required:true},
+    currency:{type:String, required:true, default:"€"},
+    colors:{type:[String], required:true},
+    sizes:{type:[String], required:true},
+    inStock:{type:Boolean, required:true, default:true}
+},
+{timestamps:true}
+);
+const ProductsSportschuhe = mongoose.model<ProductsSportschuheDocument>("ProductsSportschuhe", ProductsSportschuheSchema);
+
+export default ProductsSportschuhe;
