c533614520b42592d6204eedcaa6d425e6adf4fc
Roman <roman.rostock@gmail.com>
2022-10-05 12:20:56 +0200

    added Route productLists Sportschuhe Sneaker Women

--- backend/index.ts
+++ backend/index.ts
@@ -16,6 +16,9 @@ import * as descriptionItemRoute from './routes/DescriptionItems';
 import * as newsletterBackgroundRoute from './routes/NewsletterBackground';
 import * as productListChildRoute from './routes/ProductListChild';
 import * as productListMenRoute from './routes/ProductListMen';
+import * as productListSneakerRoute from './routes/ProductListSneaker';
+import * as productListSportschuheRoute from './routes/ProductListSportschuhe';
+import * as productListWomenRoute from './routes/ProductListWomen';
 dbConnect();
 
 app.use(cors());
@@ -30,6 +33,10 @@ app.use('/api/descriptionItem', descriptionItemRoute);
 app.use('/api/newsletterBackground', newsletterBackgroundRoute);
 app.use('/api/productListChild', productListChildRoute);
 app.use('/api/productListMen', productListMenRoute);
+app.use('/api/productListSneaker', productListSneakerRoute);
+app.use('/api/productListSportschuhe', productListSportschuheRoute);
+app.use('/api/productListWomen', productListWomenRoute);
+
 app.use(express.static(path.resolve(process.cwd(),'/frontend/public' )))
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`)
