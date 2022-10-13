c7113ff9e9bf31cd909bb50531170c15328b0333
Roman <roman.rostock@gmail.com>
2022-10-13 12:42:25 +0200

    changes for ts

--- backend/@types/index.d.ts
+++ backend/@types/index.d.ts
@@ -1,8 +1,23 @@
-import * as express from "express"
+import * as express from "express";
+export interface User {
+    id:string!;
+    vorname:string!;
+    nachname:string!;
+    username:string!;
+    email:string!;
+    street:string!;
+    number:string!;
+    plz:number!;
+    city:string!;
+    password:string!;
+    isAdmin:boolean!;
+    createdAt: Date!;
+    updatedAt: Date!;
+  }
 declare global {
     namespace Express {
         interface Request {
-            user? : Record<string>
+            user: User;
         }
     }
 }
