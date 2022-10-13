829482176be43ae9cda9b6b0a5938ac77bfdbb87
Roman <roman.rostock@gmail.com>
2022-10-13 12:42:13 +0200

    changes for ts

--- backend/middleware/jwtVerify.ts
+++ backend/middleware/jwtVerify.ts
@@ -1,38 +1,49 @@
 import * as jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express';
+
 export const verifyToken = (req:Request, res:Response, next:NextFunction)=>{
     const sec:string = process.env.JWT_SEC as string;
     const authHeader = req.headers.token;
+    console.log(authHeader);
     if(authHeader){
         const token = (authHeader as string).split(' ')[1];
-        jwt.verify(token, sec, (err, user )=>{
+        jwt.verify(token, sec, async (err, user)=>{
             if(err){
-                res.status(403).json('Der Token is nicht valide');
+                res.status(403).json('Token not valid');
             }
-            req.user = user;
+            if(!user) return res.status(400).json({error:"Not authorized"})
+            console.log(user, req.user);
+            if(user){
+               req.user = <any>user;
+            }
             next();
         })
     } else{
-        return res.status(401).json('Sie sind nicht authorisiert');
+        return res.status(401).json('Not authorized');
     }
 };
 export const verifyTokenAndAuthorization = (req:Request, res:Response, next:NextFunction)=>{
     verifyToken(req,res, ()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next();
+        if(req.user){
+            if(req.user.id === req.params.id || req.user.isAdmin){
+                next();
+            }
+            else{
+                res.status(403).json("Sie sind nicht für diese Operation authorisiert");
+            }
         }
-        else{
-            res.status(403).json("Sie sind nicht für diese Operation authorisiert");
-        }
+       
     })
 }
 export const verifyTokenAndAdmin = (req:Request, res:Response, next:NextFunction)=>{
     verifyToken(req,res, ()=>{
-        if(req.user.isAdmin){
-            next();
-        }
-        else{
-            res.status(403).json("Sie sind nicht für diese Operation authorisiert");
-        }
+        if(req.user){
+            if(req.user.isAdmin){
+                next();
+            }
+            else{
+                res.status(403).json("Sie sind nicht für diese Operation authorisiert");
+            }
+        }  
     })
 }
