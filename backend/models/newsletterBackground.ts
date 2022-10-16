63dcf0bf26dcb0d4a2fbcf1ecdc7de7159e45620
Roman <roman.rostock@gmail.com>
2022-10-16 11:34:42 +0200

    taken out cloudinary_id for testing with postman

--- backend/models/newsletterBackground.ts
+++ backend/models/newsletterBackground.ts
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 export interface NewsletterBackgroundDocument extends mongoose.Document{
     img:string;
-    cloudinary_id:string;
+    // cloudinary_id:string;
     createdAt: Date;
     updatedAt: Date;
   };
   const NewsletterBackgroundSchema = new mongoose.Schema<NewsletterBackgroundDocument>({
     img:{type:String, required:true},
-    cloudinary_id:{type:String, required:true},
+    // cloudinary_id:{type:String, required:true},
   },
   {timestamps:true},
   );
