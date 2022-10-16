15917689e26e36dee9bf37bcf1e0682a348e1a0f
Roman <roman.rostock@gmail.com>
2022-10-16 11:36:50 +0200

    prepared for testing

--- backend/models/sneakerImage.ts
+++ backend/models/sneakerImage.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 export interface SneakerImageDocument extends mongoose.Document{
     name:string;
     title:string;
-    cloudinary_id:string;
+    // cloudinary_id:string;
     img:string;
     createdAt: Date;
     updatedAt: Date;
@@ -11,7 +11,7 @@ const SneakerImageSchema = new mongoose.Schema<SneakerImageDocument>({
      name:{type:String, required:true},
      title:{type:String, required:true},
      img:{type:String, required:true},
-     cloudinary_id:{type:String, required:true},
+    //  cloudinary_id:{type:String, required:true},
 },
 {timestamps:true}
 );
