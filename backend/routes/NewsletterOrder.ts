af52f4ae242aa6f1852d0a39d558a48d1f906519
Roman <roman.rostock@gmail.com>
2023-01-25 09:31:30 +0100

    NewsletterRouter

---
+++ backend/routes/NewsletterOrder.ts
@@ -0,0 +1,37 @@
+import {Router, Request, Response} from 'express';
+const newsletterOrderRouter = Router();
+import NewsletterOrder from '../models/newsletterOrder';
+
+newsletterOrderRouter.post('/', async (req:Request, res:Response)=>{
+    const newNewsletterOrder = new NewsletterOrder(req.body);
+    try{
+        const savedNewsletterOrder = await newNewsletterOrder.save();
+        res.status(200).json(savedNewsletterOrder);
+    } catch(error){
+        res.status(403)
+        console.log(error);
+        throw new Error("Action failed")
+    }
+});
+newsletterOrderRouter.delete('/:id', async (req:Request, res:Response)=>{
+    try{
+        const deleteNewsletterOrder = await NewsletterOrder.findByIdAndDelete(req.params.id);
+        res.status(200).json(`Newsletter ${req.params.id} wurde gelöscht`);
+    } catch(error){
+        res.status(404)
+        console.log(error);
+        throw new Error("Not found")
+    }
+});
+
+newsletterOrderRouter.get('/find', async (req:Request, res:Response)=>{
+    try{
+        const allNewsletterOrders = await NewsletterOrder.find();
+        res.status(200).json(allNewsletterOrders);
+    } catch(error){
+        res.status(403)
+        console.log(error);
+        throw new Error("Action failed")
+    }
+});
+export default newsletterOrderRouter;
