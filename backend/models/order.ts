dd5f78cd95fc9dc504122a3109e9c105418841d8
Roman <roman.rostock@gmail.com>
2023-03-11 15:43:05 +0100

    created orderModel

--- backend/models/order.ts
+++ backend/models/order.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 export interface OrderDocument extends mongoose.Document{
     user:object
-    cartproduct:object;
-    amount:number;
+    netto:number;
+    amount:string;
     createdAt: Date;
     updatedAt: Date;
   };
 const OrderSchema = new mongoose.Schema<OrderDocument>({
     user:{type:Object, required:true},
-    cartproduct:{type:Object, required:true},
-    amount:{type:Number, required:true},
+    netto:{type:Number, required:true},
+    amount:{type:String, required:true},
 },
 {timestamps:true}
 );
