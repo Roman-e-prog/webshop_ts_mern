cd4677098170936f3223739b94c3a8985f7b4021
Roman <roman.rostock@gmail.com>
2023-03-11 15:42:42 +0100

    created cartModel

--- backend/models/cart.ts
+++ backend/models/cart.ts
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 export interface CartDocument extends mongoose.Document{
     user:object;
-    cartProduct:object;
-    amount:number,
+    cartProduct:object[];
+    netto:number,
+    amount:string,
     quantity:number,
     createdAt: Date;
     updatedAt: Date;
   };
 const CartSchema = new mongoose.Schema<CartDocument>({
     user:{type:Object, required:true},
-    cartProduct:{type:Object, required:true},
-    amount:{type:Number, required:true},
+    cartProduct:{type:[{type:Object}], required:true},
+    netto:{type:Number, required:true},
+    amount:{type:String, required:true},
     quantity:{type:Number, required:true},
 },
 {timestamps:true}
